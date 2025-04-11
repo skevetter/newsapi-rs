43e9b0d356f13e4a6ce7877f242660e20fcd55ed
Samuel Kevetter <69881238+OceanTrader1@users.noreply.github.com>
2025-04-11 18:39:22 -0400

    feat: implement newsapi error codes and response structure

--- src/error.rs
+++ src/error.rs
@@ -2,11 +2,49 @@ use std::error::Error;
 use std::fmt;
 
 #[derive(Debug)]
+pub enum ApiClientErrorCode {
+    ApiKeyDisabled,
+    ApiKeyExhausted,
+    ApiKeyInvalid,
+    ApiKeyMissing,
+    ParameterInvalid,
+    ParametersMissing,
+    RateLimited,
+    SourcesTooMany,
+    SourceDoesNotExist,
+    UnexpectedError,
+}
+
+#[derive(Debug)]
+pub struct ApiClientErrorResponse {
+    pub status: String,
+    pub code: ApiClientErrorCode,
+    pub message: String,
+}
+
+#[derive(Debug)]
 pub enum ApiClientError {
     Http(reqwest::Error),
     InvalidRequest(String),
-    InvalidResponse(String),
+    InvalidResponse(ApiClientErrorResponse),
     InvalidHeaderValue(reqwest::header::InvalidHeaderValue),
+}
+
+impl fmt::Display for ApiClientErrorCode {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            ApiClientErrorCode::ApiKeyDisabled => write!(f, "apiKeyDisabled"),
+            ApiClientErrorCode::ApiKeyExhausted => write!(f, "apiKeyExhausted"),
+            ApiClientErrorCode::ApiKeyInvalid => write!(f, "apiKeyInvalid"),
+            ApiClientErrorCode::ApiKeyMissing => write!(f, "apiKeyMissing"),
+            ApiClientErrorCode::ParameterInvalid => write!(f, "parameterInvalid"),
+            ApiClientErrorCode::ParametersMissing => write!(f, "parametersMissing"),
+            ApiClientErrorCode::RateLimited => write!(f, "rateLimited"),
+            ApiClientErrorCode::SourcesTooMany => write!(f, "sourcesTooMany"),
+            ApiClientErrorCode::SourceDoesNotExist => write!(f, "sourceDoesNotExist"),
+            ApiClientErrorCode::UnexpectedError => write!(f, "unexpectedError"),
+        }
+    }
 }
 
 impl fmt::Display for ApiClientError {
@@ -14,7 +52,7 @@ impl fmt::Display for ApiClientError {
         match self {
             ApiClientError::Http(err) => write!(f, "HTTP error: {}", err),
             ApiClientError::InvalidRequest(msg) => write!(f, "Invalid request: {}", msg),
-            ApiClientError::InvalidResponse(msg) => write!(f, "Invalid response: {}", msg),
+            ApiClientError::InvalidResponse(response) => write!(f, "Invalid response: {} - {}", response.code, response.message),
             ApiClientError::InvalidHeaderValue(err) => write!(f, "Invalid header value: {}", err),
         }
     }
