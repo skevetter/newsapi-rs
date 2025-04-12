9ff02c51b6db9d7bac43a354b0ef17e98964a9d3
Samuel Kevetter <69881238+OceanTrader1@users.noreply.github.com>
2025-04-12 11:17:38 -0400

    feat: remove log level constant
    Signed-off-by: Samuel Kevetter <69881238+OceanTrader1@users.noreply.github.com>

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,4 @@
-use env_logger::Env;
-use newsapi_rs::constant::DEFAULT_LOG_LEVEL;
-
 fn main() {
-    env_logger::Builder::from_env(Env::default().default_filter_or(DEFAULT_LOG_LEVEL)).init();
     println!("NewsAPI Rust Client");
     println!("Please check the examples directory for usage examples");
     println!("Run with: cargo run --example top_headlines");
