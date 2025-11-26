10204cc5c1ecc99275623b9c19c6203cb5b269e7
Samuel K <69881238+skevetter@users.noreply.github.com>
2025-11-26 00:54:48 -0500

    chore: update devcontainer (#43)
    Signed-off-by: Samuel Kevetter <69881238+skevetter@users.noreply.github.com>

--- .devcontainer/post_create.sh
+++ .devcontainer/post_create.sh
@@ -1,26 +1,14 @@
 #!/bin/bash
 
-set -e
+set -eo pipefail
 
-sudo cp .devcontainer/welcome.txt /usr/local/etc/vscode-dev-containers/first-run-notice.txt
+main() {
+    echo "Starting post-create setup in $(pwd)"
 
-sudo chown -R vscode:vscode /cmd_history
+    sudo cp ".devcontainer/welcome.txt" "/usr/local/etc/vscode-dev-containers/first-run-notice.txt"
+    pre-commit install --install-hooks || { echo "Pre-commit hooks failed to install"; }
 
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
+    echo "Post-create setup complete!"
+}
 
-sudo apt update && sudo apt install pkg-config -y
-cargo build && cargo test --all-features
-
-sudo ln -s /home/vscode/.cargo/bin/cargo /usr/bin/cargo
-
-pre-commit install --install-hooks
-pre-commit run --all-files
-
-TMP_DIR=$(mktemp -d)
-cd "$TMP_DIR"
-LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": *"v\K[^"]*')
-curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/download/v${LAZYGIT_VERSION}/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
-tar xf lazygit.tar.gz lazygit
-sudo install lazygit -D -t /usr/local/bin/
-cd - >/dev/null
-rm -rf "$TMP_DIR"
+main "$@"
